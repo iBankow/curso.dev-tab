bdcec8091271e9f65ad23870fc36780146a8a5c0
Tiago Bankow <bkw.designer@gmail.com>
2024-09-27 13:56:59 +0000

    finaliza o endpoint `/api/v1/status`

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,8 +1,23 @@
 import database from "infra/database.js";
 
 export default async function status(request, response) {
-  const result = await database.query("SELECT 1 + 1 as sum;");
-  console.log(result.rows);
+  const updateAt = new Date().toISOString();
+  const version = await database.query("SHOW server_version;");
+  const maxConnections = await database.query("SHOW max_connections;");
+  const activityConnection = await database.query(
+    "SELECT COUNT(*)::int FROM pg_stat_activity WHERE datname = $1;",
+    [process.env.POSTGRES_DB],
+  );
 
-  response.status(200).json({ api: "confia" });
+  response.status(200).json({
+    updated_at: updateAt,
+    api: "confia",
+    dependencies: {
+      database: {
+        version: version.rows[0].server_version,
+        max_connections: parseInt(maxConnections.rows[0].max_connections),
+        opened_connections: activityConnection.rows[0].count,
+      },
+    },
+  });
 }
