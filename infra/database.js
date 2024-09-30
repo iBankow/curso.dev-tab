7106537e17017caf81c628acfee151388ecbee97
Tiago Bankow <bkw.designer@gmail.com>
2024-09-30 15:25:51 +0000

    add mais robustez no metodo de configuração ssl

--- infra/database.js
+++ infra/database.js
@@ -7,7 +7,7 @@ async function query(queryObject, params) {
     database: process.env.POSTGRES_DB,
     user: process.env.POSTGRES_USER,
     password: process.env.POSTGRES_PASSWORD,
-    ssl: process.env.NODE_ENV === "development" ? false : true,
+    ssl: getSSLValues(),
   });
   try {
     await client.connect();
@@ -19,6 +19,16 @@ async function query(queryObject, params) {
   } finally {
     await client.end();
   }
+}
+
+function getSSLValues() {
+  if (process.env.POSTGRES_CA) {
+    return {
+      ca: process.env.POSTGRES_CA,
+    };
+  }
+
+  return process.env.NODE_ENV === "development" ? false : true;
 }
 
 export default { query };
