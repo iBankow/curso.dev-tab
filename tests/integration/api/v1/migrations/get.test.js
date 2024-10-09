f135d60d9c0f5a0109085f84fbad63749d075b22
Tiago Bankow <bkw.designer@gmail.com>
2024-10-09 18:13:04 +0000

    refactor: use `orchestrator.clearDatabase()` in tests

--- tests/integration/api/v1/migrations/get.test.js
+++ tests/integration/api/v1/migrations/get.test.js
@@ -1,14 +1,9 @@
 import orchestrator from "tests/orchestrator.js";
-import database from "infra/database";
 
 beforeAll(async () => {
   await orchestrator.waitForAllServices();
-  await cleanDatabase();
+  await orchestrator.clearDatabase();
 });
-
-async function cleanDatabase() {
-  await database.query("drop schema public cascade; create schema public;");
-}
 
 describe("GET /api/v1/migrations", () => {
   describe("Anonymous user", () => {
