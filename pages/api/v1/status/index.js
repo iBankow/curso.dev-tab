6be8b7c2cfbc0ed6fe6451a4c985a24190eba1a0
Tiago Bankow <bkw.designer@gmail.com>
2025-01-10 18:39:45 +0000

    feat: create and use `InternalServerError` in `/api/v1/status`

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,23 +1,35 @@
 import database from "infra/database.js";
+import { InternalServerError } from "infra/error";
 
 export default async function status(request, response) {
-  const updateAt = new Date().toISOString();
-  const version = await database.query("SHOW server_version;");
-  const maxConnections = await database.query("SHOW max_connections;");
-  const activityConnection = await database.query(
-    "SELECT COUNT(*)::int FROM pg_stat_activity WHERE datname = $1;",
-    [process.env.POSTGRES_DB],
-  );
+  try {
+    const updateAt = new Date().toISOString();
+    const version = await database.query("SHOW server_version;");
+    const maxConnections = await database.query("SHOW max_connections;");
+    const activityConnection = await database.query(
+      "SELECT COUNT(*)::int FROM pg_stat_activity WHERE datname = $1;",
+      [process.env.POSTGRES_DB],
+    );
 
-  response.status(200).json({
-    updated_at: updateAt,
-    api: "confia",
-    dependencies: {
-      database: {
-        version: version.rows[0].server_version,
-        max_connections: parseInt(maxConnections.rows[0].max_connections),
-        opened_connections: activityConnection.rows[0].count,
+    response.status(200).json({
+      updated_at: updateAt,
+      api: "confia",
+      dependencies: {
+        database: {
+          version: version.rows[0].server_version,
+          max_connections: parseInt(maxConnections.rows[0].max_connections),
+          opened_connections: activityConnection.rows[0].count,
+        },
       },
-    },
-  });
+    });
+  } catch (error) {
+    const publicErrorObject = new InternalServerError({
+      cause: error,
+    });
+
+    console.log("\n Erro dentro do catch do controller:");
+    console.error(publicErrorObject);
+
+    response.status(500).json(publicErrorObject);
+  }
 }
